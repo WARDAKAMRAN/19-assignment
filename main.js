22aecc8df7d9cbe2603ef448d43926bda39e8990
Warda Kamran <wkamran523@gmail.com>
2024-03-13 08:21:39 -0700

    19,assignment

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var main_1 = require("../14.ASSIGNMENT/main");
+console.log("n printing number of invited");
+console.log("we are finally invited people ".concat(main_1.guest_list.length, " from exercise 14 "));
---
+++ main.ts
@@ -0,0 +1,3 @@
+import{ guest_list } from '../14.ASSIGNMENT/main'
+console.log(`n\ printing number of invited`)
+console.log(`we are finally invited people ${guest_list.length} from exercise 14 `)
